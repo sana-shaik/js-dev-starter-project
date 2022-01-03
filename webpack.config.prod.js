e5974092ad016ac74a05d94eba51d41162a15820
Shaik,Sanaulla <Sanaulla.Shaik@Cerner.com>
2022-01-03 18:38:54 +0530

    minification sourcemaps cachebusting bundle splitting

---
+++ webpack.config.prod.js
@@ -0,0 +1,35 @@
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import path from "path";
+// import * as HtmlWebpackPlugin from "html-webpack-plugin";
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+export default {
+  mode: "production",
+  devtool: "source-map",
+  entry: {
+    main: path.resolve(__dirname, "src/index"),
+    vendor: path.resolve(__dirname, "src/vendor"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].[chunkhash].js",
+  },
+  plugins: [
+    //Generate an external css file with a hash in the filename.
+    new MiniCssExtractPlugin({
+      filename: "[name].[chunkhash].css",
+    }),
+
+    // Create HTML file that includes reference to bundled JS.
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      { test: /\.js$/, exclude: /node_modules/, use: ["babel-loader"] },
+      { test: /\.css$/, use: [MiniCssExtractPlugin.loader, "css-loader"] },
+    ],
+  },
+};
